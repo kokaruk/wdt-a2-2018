62ab9e02937e4b39f81b9d777fe7eddcee7fa905
dimz <kokaruk@gmail.com>
2019-01-05 13:48:46 +1100

    API initial work

---
+++ WdtA2Api/Data/WdtA2ApiProductsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WdtA2Api.Models
+{
+    public class WdtA2ApiProductsContext : DbContext
+    {
+        public WdtA2ApiProductsContext (DbContextOptions<WdtA2ApiProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WdtA2Api.Models.Product> Product { get; set; }
+    }
+}
---
+++ WdtA2Api/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WdtA2Api.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
